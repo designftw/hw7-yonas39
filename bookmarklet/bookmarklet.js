50b964d5756662c4f26621c6a95f34c268113bad
yonas39 <124627215+yonas39@users.noreply.github.com>
2023-04-05 18:31:07 -0400

    bookmarklet completed

--- bookmarklet/bookmarklet.js
+++ bookmarklet/bookmarklet.js
@@ -1,1 +1,15 @@
-
+(function () {
+  var inputs = document.querySelectorAll("input");
+  inputs.forEach(function (input) {
+    if (
+      input.type === "password" ||
+      input.getAttribute("data-toggle-password") === "true"
+    ) {
+      input.type = input.type === "password" ? "text" : "password";
+      input.setAttribute(
+        "data-toggle-password",
+        input.type === "password" ? "false" : "true"
+      );
+    }
+  });
+})();
